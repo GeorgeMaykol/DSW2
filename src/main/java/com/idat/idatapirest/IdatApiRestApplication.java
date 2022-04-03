295f0640aae213a536620e30474b5dfc5385d13c
Maykol Rojas <maykolroj@gmail.com>
2022-04-03 07:13:40 -0500

    initial

---
+++ src/main/java/com/idat/idatapirest/IdatApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.idat.idatapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdatApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdatApiRestApplication.class, args);
+	}
+
+}
